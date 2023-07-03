7ccc36f670d502e21f86f0f01943bf20b2c2d8fb
Pavel <19418601+rakleed@users.noreply.github.com>
2023-07-03 06:33:10 +0300

    Fix tests

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { notFoundError } = require('./errors/errors');
+const { NOT_FOUND } = require('./errors/errors');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
-app.use((req, res, next) => {
+app.use((req, next) => {
   req.user = {
     _id: '64a082113db0edce5dfcf98e',
   };
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => res.status(notFoundError).send({ message: 'Такая страница не существует.' }));
+app.use('*', (res) => res.status(NOT_FOUND).send({ message: 'Такая страница не существует.' }));
 
 app.listen(3000);
