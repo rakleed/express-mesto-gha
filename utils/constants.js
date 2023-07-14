073a1cef7c4fd544c59d1a638b06946ac9e9537b
Pavel <19418601+rakleed@users.noreply.github.com>
2023-07-14 17:43:15 +0300

    Update MongoDB connection URL

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-module.exports.MONGODB_URL = 'mongodb://localhost:27017/mestodb';
+module.exports.MONGODB_URL = 'mongodb://127.0.0.1:27017/mestodb';
 module.exports.PORT = 3000;
 module.exports.SECRET_PASSWORD_KEY = 'some-secret-key';
 
